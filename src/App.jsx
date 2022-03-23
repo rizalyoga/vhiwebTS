4d1487c3fd66e7e146fb7a8b2e9df8345b689ea2
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-03-24 06:29:39 +0700

    add cover

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home";
 import Users from "./pages/Users/Users";
 import UsersDetail from "./pages/Users/DetailUser";
 import NotFoundPage from "./pages/404/NotFound";
+import Cover from "./pages/Cover/Cover";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cover" element={<Cover />} />
         <Route path="/users" element={<Users />} />
         <Route path="/users/:id" element={<UsersDetail />} />
         <Route path="*" element={<NotFoundPage />} />
---
+++ src/pages/Cover/Cover.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cover = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-slate-300">
+      <h1 className="font-logo text-8xl text-gray-800">Employee</h1>
+    </div>
+  );
+};
+
+export default Cover;
