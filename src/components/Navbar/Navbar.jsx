7e4872fd0021a2637d33cfe9881168a43d86707f
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-03-23 22:17:05 +0700

    fix layout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -38,20 +38,20 @@ const Navbar = () => {
     return (
       <nav className="fixed top-0 bg-gray-300 h-14 w-full flex items-center">
         <div className="row wraper mx-auto px-3 lg:px-0 w-full flex  ">
-          <div className="left-col w-6/12 flex items-center gap-5">
+          <div className="left-col w-9/12 md:w-6/12 flex items-center gap-5">
             <h1 className="font-logo tracking-wider text-gray-700 cursor-pointer font-bold text-xl lg:text-2xl ">Employee</h1>
             <div className="flex list-menu items-center gap-3 lg:mt-2 mt-1 ">
-              <p onClick={toHome} className="cursor-pointer hover:text-white active:text-black transition ease-in">
+              <button onClick={toHome} className="cursor-pointer hover:text-white active:text-black transition ease-in">
                 Home
-              </p>
-              <p onClick={toUsers} className="cursor-pointer hover:text-white active:text-black transition ease-in">
+              </button>
+              <button onClick={toUsers} className="cursor-pointer hover:text-white active:text-black transition ease-in">
                 Users
-              </p>
+              </button>
             </div>
           </div>
 
-          <div className="right-col w-6/12 flex justify-end">
-            <button className="lg:mt-1 hover:text-white active:text-black transition ease-in" onClick={logOut}>
+          <div className="right-col w-3/12 md:w-6/12 flex justify-end">
+            <button className="mt-1 lg:mt-1 hover:text-white active:text-black transition ease-in" onClick={logOut}>
               Log Out
             </button>
           </div>
